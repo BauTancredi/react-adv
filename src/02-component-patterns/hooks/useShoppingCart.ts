2a203d770abef0a1c714d1c8cfd8277635d21905
BauTancredi <bautistatancredi@hotmail.com>
2022-02-09 17:09:52 -0300

    Component Control Props alternative ready

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,24 +6,15 @@ const useShoppingCart = () => {
 
   const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
     setShoppingCart((oldShoppingCard) => {
-      const productInCart: ProductInCart = oldShoppingCard[product.id] || { ...product, count: 0 };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-
-        return {
-          ...oldShoppingCard,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCard;
+        return rest;
       }
 
-      const { [product.id]: toDelete, ...rest } = oldShoppingCard;
-      return rest;
-
-      // return {
-      //   ...oldShoppingCard,
-      //   [product.id]: { ...product, count },
-      // };
+      return {
+        ...oldShoppingCard,
+        [product.id]: { ...product, count },
+      };
     });
   };
 
