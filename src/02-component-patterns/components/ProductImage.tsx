4e1098e38e765c548d251676e78521dc33c24f14
BauTancredi <bautistatancredi@hotmail.com>
2022-02-09 09:50:57 -0300

    Component Extend Styles ready

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,11 +1,24 @@
-import { useContext } from "react";
+import { CSSProperties, useContext } from "react";
 import { ProductContext } from "./ProductCard";
 
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 
-export const ProductImage = ({ img = "" }) => {
+export interface Props {
+  className?: string;
+  img?: string;
+  style?: CSSProperties;
+}
+
+export const ProductImage = ({ img = "", className, style }: Props) => {
   const { product } = useContext(ProductContext);
 
-  return <img className={styles.productImg} src={img || product.img || noImage} alt="Product" />;
+  return (
+    <img
+      className={`${styles.productImg} ${className}`}
+      src={img || product.img || noImage}
+      style={style}
+      alt="Product"
+    />
+  );
 };
